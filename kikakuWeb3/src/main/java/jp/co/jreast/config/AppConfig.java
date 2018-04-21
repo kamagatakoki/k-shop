79b799dffd009d7e18fd11ff159cc51114668ffc
kamagata <kamagata@DESKTOP-T23A6V0.mshome.net>
2018-04-22 02:29:30 +0900

    2018/04/22

--- kikakuWeb3/src/main/java/jp/co/jreast/config/AppConfig.java
+++ kikakuWeb3/src/main/java/jp/co/jreast/config/AppConfig.java
@@ -9,41 +9,5 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class AppConfig {
-	//
-	//	/**
-	//	 * メソッドの説明：Tiles用のViewResolver
-	//	 * @author kamagata
-	//	 * @since 2018/04/22
-	//	 * @param urlBasedViewResolver a
-	//	 */
-	//	@Bean
-	//	public void tilesViewResolver(UrlBasedViewResolver urlBasedViewResolver) {
-	//		urlBasedViewResolver.setViewClass(TilesView.class);
-	//		urlBasedViewResolver.setOrder(0);
-	//	}
-	//
-	//	/**
-	//	 * メソッドの説明：元々のViewResolver
-	//	 * @author kamagata
-	//	 * @since 2018/04/22
-	//	 * @param internalResourceViewResolver a
-	//	 */
-	//	@Bean
-	//	public void viewResolver(InternalResourceViewResolver internalResourceViewResolver) {
-	//		internalResourceViewResolver.setPrefix("/WEB-INF/views");
-	//		internalResourceViewResolver.setSuffix(".jsp");
-	//		internalResourceViewResolver.setOrder(1);
-	//	}
-	//
-	//	/**
-	//	 * メソッドの説明：Tilesの定義ファイルの指定
-	//	 * @author kamagata
-	//	 * @since 2018/04/22
-	//	 * @param tilesConfigurer a
-	//	 */
-	//	@Bean
-	//	public void tilesConfigurer(TilesConfigurer tilesConfigurer) {
-	//		tilesConfigurer.setDefinitions("/WEB-INF/conf/tiles-defs.xml");
-	//
-	//	}
+
 }
--- kikakuWeb3/src/main/java/jp/co/jreast/config/WebMvcConfig.java
+++ kikakuWeb3/src/main/java/jp/co/jreast/config/WebMvcConfig.java
@@ -1,11 +1,16 @@
 package jp.co.jreast.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 /**
  * クラスの説明：
@@ -26,6 +31,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry viewResolverRegistry) {
 		viewResolverRegistry.jsp().prefix("/WEB-INF/jsp/");
+		// Tiles ViewResolver
+		TilesViewResolver viewResolver = new TilesViewResolver();
+		viewResolverRegistry.viewResolver(viewResolver);
 	}
 
 	/** オーバーライドの説明：
@@ -40,4 +48,35 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/resources/**")
 				.addResourceLocations("classpath:../../resources/");
 	}
+
+	/**
+	 * メソッドの説明：
+	 * @author kamagata
+	 * @since 2018/04/22
+	 * @return na
+	 */
+	@Bean
+	public MessageSource MessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		// クラスパス上に格納されているプロパティファイルを指定する
+		messageSource.setBasename("appresources.properties");
+		return messageSource;
+	}
+
+	/**
+	 * メソッドの説明：Tiles定義ファイル読込
+	 * @author kamagata
+	 * @since 2018/04/22
+	 * @return a
+	 */
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer tilesConfigurer = new TilesConfigurer();
+		tilesConfigurer.setDefinitions(
+				new String[] { "/WEB-INF/conf/tiles-defs.xml" });
+		tilesConfigurer.setCheckRefresh(true);
+
+		return tilesConfigurer;
+	}
+
 }
